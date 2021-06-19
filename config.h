6b4d935acb2839fb3612620686c6a3b55e0580f3
LoanPa <64327463+LoanPa@users.noreply.github.com>
2021-06-19 11:55:13 +0000

    Default terminal set to urxvt

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 /* brightness */
 static const char *brightnessupcmd[] = { "brightnessctl", "set", "10%+", NULL };
